5dafec1d36004416ea81bbb0c41f00f7102d12e6
Jesus <Jesus@PrincipalGP>
2014-09-21 19:08:44 -0600

    Fase1. v0.1

---
+++ HorarioFIUSAC/js/graficar.js
@@ -0,0 +1,178 @@
+var dias = ["Hora", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
+var colores = ["Blue", "Red", "Green", "Orange", "Purple", "Yellow", "Gray", "Olive", "Aqua", "PowderBlue", "Brown", "White", "Crimson", "Chocolate", "Tan", "Teal"];
+
+function graficar(margen) {
+    var filWidth = 60;
+    var orderHorario = crearListaHorario();
+    var filas = orderHorario.size;
+
+    var escenario = new Kinetic.Stage({
+        width: 1000,
+        height: 40 + filas * filWidth + margen * 2,
+        container: 'canvasHorario'
+    });
+
+    var malla = new Kinetic.Layer({
+        x: margen,
+        y: margen,
+        width: escenario.getWidth() - margen * 2,
+        height: escenario.getHeight() - margen * 2
+                /*draggable: true*/
+    });
+
+    var colWidth = malla.getWidth() / 8;
+
+    //Fondo
+    var rect = new Kinetic.Rect({
+        width: malla.getWidth(),
+        height: malla.getHeight(),
+        fill: "#E6E6E6",
+        cornerRadius: 5,
+        shadowColor: 'black',
+        shadowBlur: 15,
+        shadowOffset: {
+            x: 5,
+            y: 5
+        },
+        shadowOpacity: 0.4 //strokeWidth: 1, opacity: 0.2, stroke: "#000",
+    });
+    malla.add(rect);
+
+    //Lineas horizontales
+    for (i = 0; i < filas; i++) {
+        if (i === 0) {
+            var line = new Kinetic.Line({
+                points: [0, 40, malla.getWidth(), 40],
+                stroke: 'black',
+                strokeWidth: 1.5,
+                lineCap: 'miter',
+                lineJoin: 'round'
+            });
+            malla.add(line);
+        } else {
+            var line = new Kinetic.Line({
+                points: [0, 40 + filWidth * i, malla.getWidth(), 40 + filWidth * i],
+                stroke: 'gray',
+                strokeWidth: 1,
+                lineCap: 'miter',
+                lineJoin: 'round'
+                        //dash: [10, 5]
+            });
+            malla.add(line);
+        }
+    }
+
+    //Lineas verticales
+    for (i = 1; i <= 7; i++) {
+        var line = new Kinetic.Line({
+            x: 0,
+            y: 0,
+            points: [colWidth * i, 0, colWidth * i, malla.getHeight()],
+            stroke: 'gray',
+            strokeWidth: 1,
+            lineCap: 'miter',
+            lineJoin: 'round',
+            dash: [10, 5, 2, 5]
+        });
+        malla.add(line);
+    }
+
+    //Textos Dias
+    for (i = 0; i < dias.length; i++) {
+        var texto = new Kinetic.Text({
+            x: (colWidth * (i + 1)) - (colWidth / 2),
+            y: 10,
+            text: dias[i],
+            fontSize: 20,
+            fontFamily: 'Calibri',
+            strokeWidth: 5,
+            fill: 'green'
+        });
+        texto.offsetX(texto.width() / 2);
+        malla.add(texto);
+    }
+
+    //Textos Horas y Cursos
+    var aux = orderHorario.primero;
+    var i = 1;
+    var m = 0;
+    while (aux !== undefined) {
+        var h1 = aux.dato[0].toString();
+        var h2 = aux.dato[1].toString();
+        h1 = h1.substr(0, h1.length - 2) + ":" + h1.substr(h1.length - 2, h1.length);
+        h2 = h2.substr(0, h2.length - 2) + ":" + h2.substr(h2.length - 2, h2.length);
+
+        var texto = new Kinetic.Text({
+            x: colWidth / 2,
+            y: 40 + filWidth * i - (filWidth / 2),
+            text: h1 + " - " + h2,
+            fontSize: 16,
+            fontFamily: 'Calibri',
+            strokeWidth: 5,
+            fill: 'black'
+        });
+        texto.offsetX(texto.width() / 2);
+        texto.offsetY(texto.height() / 2);
+        malla.add(texto);
+
+        //Texto Cursos
+        for (j = 0; j < aux.dato[2].length; j++) {
+            var cursos = aux.dato[2];
+            var curso = cursos[j];            
+            m++;
+            
+            for (k = 0; k < diasR.length; k++) {
+                if (curso[diasR[k]] === true) {
+                    //Fondo
+                    var rect = new Kinetic.Rect({
+                        x : colWidth * (k + 1) + 5,
+                        y : 40 + filWidth * (i - 1) + 5,
+                        width: colWidth - 10,
+                        height: filWidth - 10,
+                        fill: colores[m],
+                        cornerRadius: 1,
+                        opacity: 0.7
+                    });
+                    malla.add(rect);
+                    
+                    //Texto
+                    var texto = new Kinetic.Text({
+                        x: (colWidth * (k + 2)) - (colWidth / 2),
+                        y: 40 + filWidth * i - (filWidth / 2),
+                        text: curso.name,
+                        fontSize: 16,
+                        fontFamily: 'Calibri',
+                        strokeWidth: 5,
+                        fill: 'black'
+                    });
+                    texto.offsetX(texto.width() / 2);
+                    texto.offsetY(texto.height() / 2);
+                    malla.add(texto);                  
+                }
+            }
+        }
+
+        aux = aux.sig;
+        i++;
+    }
+
+    escenario.add(malla);
+    escenario.draw();
+    $("#canvasHorario canvas").css("position", "absolute");
+    //$("#mi canvas").css("border", "1px solid black");
+    //$("#mi canvas").removeAttr('style');
+}
+
+function crearListaHorario() {
+    var l = new ltshOrdenado();
+
+    var aux = ltsHorario.primero;
+    while (aux !== undefined) {
+        if (aux.dato[1] === true) {
+            l.agregar(aux.dato[0]);
+        }
+        aux = aux.sig;
+    }
+
+    return l;
+}
---
+++ HorarioFIUSAC/js/manejador.js
@@ -0,0 +1,232 @@
+var arrCursos = [];
+var ltsHorario = new ListaHorario();
+var selCurso = undefined;
+var askAgregar = true;
+
+function addCurso(cod, name, seccion, edificio, salon, inicio, fin, lu, ma, mi, ju, vi, sa, dom) {
+    return arrCursos.push(new Curso(arrCursos.length, cod, name, seccion, edificio, salon, inicio, fin, lu, ma, mi, ju, vi, sa, dom));
+}
+
+function getCurso(id) {
+    if (arrCursos[id] !== undefined) {
+        return arrCursos[id];
+    } else {
+        return undefined;
+    }
+}
+
+//Agregar Cursos prueba
+for (i = 1; i <= 5; i++) {
+    addCurso("07" + i, "Curso" + i, "A", "T3", "105", 700, 800, false, true, false, true, false, false, false);
+}
+addCurso("076", "Curso6", "A", "T3", "105", 630, 730, false, true, false, true, false, false, false);
+addCurso("077", "Curso7", "A", "T3", "105", 800, 900, false, true, false, true, false, false, false);
+addCurso("078", "Curso8", "A", "T3", "105", 730, 800, true, false, false, false, false, false, false);
+addCurso("079", "Curso9", "A", "T3", "105", 700, 800, false, false, true, false, true, false, false);
+
+//Al estar listo el documento
+$(document).ready(function() {
+
+    //Agregando Cursos a Tabla Lista
+    for (i = 0; i < arrCursos.length; i++) {
+        var curso = arrCursos[i];
+        var fila = $("<tr></tr>").attr("id", i).addClass("drag_curso");
+
+        $("<td></td>").text(curso.cod).appendTo($(fila));
+        $("<td></td>").text(curso.name).appendTo($(fila));
+        $("<td></td>").text(curso.seccion).appendTo($(fila));
+        $("<td></td>").text(curso.edificio).appendTo($(fila));
+        $("<td></td>").text(curso.salon).appendTo($(fila));
+        $("<td></td>").text(curso.inicio).appendTo($(fila));
+        $("<td></td>").text(curso.fin).appendTo($(fila));
+        $("<td></td>").text(boolToX(curso.lu)).appendTo($(fila));
+        $("<td></td>").text(boolToX(curso.ma)).appendTo($(fila));
+        $("<td></td>").text(boolToX(curso.mi)).appendTo($(fila));
+        $("<td></td>").text(boolToX(curso.ju)).appendTo($(fila));
+        $("<td></td>").text(boolToX(curso.vi)).appendTo($(fila));
+        $("<td></td>").text(boolToX(curso.sa)).appendTo($(fila));
+        $("<td></td>").text(boolToX(curso.dom)).appendTo($(fila));
+        fila.appendTo($("#tablaCursos"));
+    }
+
+    function boolToX(valor) {
+        if (valor === true) {
+            return "x";
+        } else {
+            return "-";
+        }
+    }
+
+    //Ventana Horario
+    $("#horario").dialog({
+        autoOpen: true,
+        draggable: true,
+        title: "Mi Horario :)",
+        minHeight: 40,
+        minWidth: 5,
+        height: 300,
+        width: 600,
+        dialogClass: "mi_horario",
+        buttons: [{
+                text: "Ver",
+                click: function() {
+                    graficar(50);
+                    $("#msgHorario").dialog("open");                    
+                }}]
+    }).dialogExtend({
+        closable: false,
+        maximizable: false,
+        minimizable: true,
+        titlebar: false,
+        minimizeLocation: "right"
+    });
+    $("#horario").dialog("option", "position", {my: "right-3% bottom-5%", at: "right bottom", of: window});
+
+    //Ventana canvas horario
+    $("#msgHorario").dialog({
+        modal: true,
+        autoOpen: false,
+        draggable: false,
+        resizable: false,
+        height: "auto",
+        width: "auto",
+        dialogClass: "pr"
+    }).dialogExtend({
+        closable: true,
+        titlebar: "none",
+        minimizeLocation: "right"
+    });
+
+    //Ventana Mensaje
+    $("#mensaje").dialog({
+        autoOpen: false,
+        draggable: false,
+        resizable: false,
+        height: 200,
+        width: 300
+    });
+
+    //Drag Horario definitivo
+    $(".drag_curso").draggable({
+        /*appendTo: "body",*/
+        helper: "clone",
+        cursor: "move",
+        opacity: 0.5,
+        cursorAt: {left: 5}
+    });
+
+    //Mostrar Mensaje
+    function showMsg(p_modal, titulo, texto, botones) {
+        $("#msg_texto").text(texto);
+        $("#mensaje").dialog("option", "modal", p_modal);
+        $("#mensaje").dialog("option", "title", titulo);
+        $("#mensaje").dialog("option", "buttons", botones);
+        $("#mensaje").dialog("open");
+    }
+
+    //Agregar Normal
+    function addNormal(curso) {
+        switch (ltsHorario.addCursoHorario([curso, true])) {
+            case 0:
+                $("#lista_def").find(".placeholder").remove();
+                $("<li></li>").text(curso.name).appendTo($("#lista_def"));
+                console.log("Curso Agregado.");
+                break;
+            case 1:
+                showMsg(true, "Error ...", "Curso ya agregado.", [{text: "Ok", click: function() {
+                            $(this).dialog("close");
+                        }}]);
+                console.log("Ya se ha agregado el curso.");
+                break;
+            case 2:
+                showMsg(true, "Traslape ...", "Exite un traslape", [
+                    {
+                        text: "Ok",
+                        click: function() {
+                            $(this).dialog("close");
+                        }
+                    },
+                    {
+                        text: "como Posible",
+                        click: function() {
+                            $(this).dialog("close");
+                            addPosible(curso);
+                        }
+                    }]);
+                console.log("Existe traslape de curso.");
+                break;
+            default:
+                showMsg(true, "Error ...", "No se pudo agregar.", [{text: "Ok", click: function() {
+                            $(this).dialog("close");
+                        }}]);
+                console.log("Error al agregar.");
+        }
+    }
+
+    //Agregar Posible
+    function addPosible(curso) {
+        $("#lista_pos").find(".placeholder").remove();
+        $("<li></li>").text(curso.name).appendTo($("#lista_pos"));
+        ltsHorario.addCursoHorario([curso, false]);
+    }
+
+    /*Drop sobre horario*/
+    $("#horario").droppable({
+        activeClass: "ui-state-default",
+        hoverClass: "ui-state-hover",
+        accept: ":not(.ui-sortable-helper)",
+        drop: function(event, ui) {
+            selCurso = getCurso(ui.draggable.attr("id"));
+
+            if (selCurso !== undefined) {
+                if (ltsHorario.find(selCurso.id) === undefined) {
+                    if (askAgregar === true) {
+                        showMsg(true, "Agergar Curso ...", "Como desea agergar el curso?", [
+                            {
+                                text: "Normal",
+                                click: function() {
+                                    $(this).dialog("close");
+                                    addNormal(selCurso);
+                                }
+                            },
+                            {
+                                text: "Posible",
+                                click: function() {
+                                    $(this).dialog("close");
+                                    addPosible(selCurso);
+                                }
+                            }]);
+                    } else {
+                        addNormal(selCurso);
+                    }
+                } else {
+                    showMsg(true, "Error ...", "Curso ya agregado.", [{text: "Ok", click: function() {
+                                $(this).dialog("close");
+                            }}]);
+                }
+            } else {
+                showMsg(true, "Error ...", "No encontrado.", [{text: "Ok", click: function() {
+                            $(this).dialog("close");
+                        }}]);
+            }
+        }
+    });
+
+    $("#lista_def").sortable({
+        items: "li:not(.placeholder)",
+        cursor: "move",
+        opacity: 0.5,
+        sort: function(event, ui) {
+            $(this).removeClass("ui-state-default");
+        }
+    });
+
+    $("#lista_pos").sortable({
+        items: "li:not(.placeholder)",
+        cursor: "move",
+        opacity: 0.5,
+        sort: function(event, ui) {
+            $(this).removeClass("ui-state-default");
+        }
+    });
+});
